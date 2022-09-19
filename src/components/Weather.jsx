fac08dde3b267515e551eed60bf1fbabba295ed1
iarmolla <ivan.armolla@gmail.com>
2022-09-19 03:58:22 -0300

    renaming the state

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -6,11 +6,11 @@ import WeatherCard from './WeatherCard'
 function Weather() {
   const [weather, updateWeather] = useState({ "coord": { "lon": -64, "lat": -34 }, "weather": [{ "id": 803, "main": "Clouds", "description": "broken clouds", "icon": "04n" }], "base": "stations", "main": { "temp": 10.86, "feels_like": 10.06, "temp_min": 10.86, "temp_max": 10.86, "pressure": 1014, "humidity": 79, "sea_level": 1014, "grnd_level": 992 }, "visibility": 10000, "wind": { "speed": 8.13, "deg": 233, "gust": 11.22 }, "clouds": { "all": 78 }, "dt": 1663540542, "sys": { "type": 2, "id": 2040186, "country": "AR", "sunrise": 1663495868, "sunset": 1663538959 }, "timezone": -10800, "id": 3865483, "name": "Argentina", "cod": 200 })
   const [country, updateCountry] = useState('')
-  const [test, updateTest] = useState([])
+  const [allCountries, updateAllCountries] = useState([])
   useEffect(() => {
-    updateTest([...test, weather])
+    updateAllCountries([...allCountries, weather])
   }, [weather])
-  const handleChange = async() => {
+  const handleChange = () => {
     getWeatherByCountry(updateWeather, country)
   }
   return (
@@ -36,7 +36,7 @@ function Weather() {
       <div className="flex justify-center content-center">
         <div className="grid grid-cols-1 gap-4 mt-5 md:grid md:grid-cols-3 md:gap-5 md:mt-8 md:mx-4 max-w-4xl ">
           {
-            test.map((weather) => {
+            allCountries.map((weather) => {
               return <div key={weather.id}>
                 <WeatherCard weather={weather}></WeatherCard>
               </div>
